0e620cd95e780dc197ad489712fe550ea2a62d19
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-01-27 17:49:00 -0300

    Correção exercício orientação a objetos

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
 //Classe abstração:
 
-function VideoGame(modelo, fabricante, anoLancamento, nome, preco) {
+function VideoGame(modelo, fabricante, jogo, joystick, precoAtual) {
     this.modelo = modelo;
     this.fabricante = fabricante;
-    this.anoLancamento = anoLancamento;
-    this.comprou = function() {
-        console.log(this.nome + " Comprou um novo video game")
-    }
-
-    Consumidor.call(this, nome)
-    Preco.call(this, preco)
+    
+    Jogos.call(this, jogo);
+    Acessorio.call(this, joystick);
+    Price.call(this, precoAtual);
 }
 
 //Classes herdeiras:
 
-function Consumidor(nome) {
-    this.nome = nome;
+function Jogos(jogo) {
+    this.jogo = jogo;
 }
 
-function Preco(preco) {
-    this.preco = preco;
+function Acessorio(joystick) {
+    this.joystick = joystick;
+}
+
+function Price(precoAtual) {
+    this.precoAtual = precoAtual;
 }
 
 //Instâncias de objetos
 
-const cliente1 = new VideoGame("PLaystation 5", "Sony", 2020, "Leonardo", 4000)
+const comobo1 = new VideoGame("PLaystation 5", "Sony", "God of War", "Dualsense", 4.499);
+console.log(comobo1);
 
-const cliente2 = new VideoGame("Xbox One", "Microsoft", 2013, "Maria", 2000)
+const comobo2 = new VideoGame("Xbox One", "Microsoft", "Halo Infinite", "Elite", 2.355);
+console.log(comobo2);
 
-const cliente3 = new VideoGame("Nintendo Switch", "Nintendo", 2017, "Jade", 2.899)
-
-
-console.log(cliente1.comprou())
-console.log(cliente1)
-
-console.log(cliente2.comprou())
-console.log(cliente2)
-
-console.log(cliente3.comprou())
-console.log(cliente3)
+const comobo3 = new VideoGame("Nintendo Switch", "Nintendo", "Super Mario World", "Joycon", 2.899);
+console.log(comobo3);
