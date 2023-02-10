875ac94ea58ca6b89438d2d6a46e330051df005e
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-02-10 16:52:21 -0300

    tarefa typescript

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+//FUNÇÃO MULTIPLICAÇÃO COM ARROW FUNCTION:
+const multiplicaNumeros = (numero1, numero2) => numero1 * numero2;
+console.log(multiplicaNumeros(5, 8));
+//FUNÇÃO DE SAUDAÇÃO:
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao('Jade'));
---
+++ src/exercicio.ts
@@ -0,0 +1,12 @@
+//FUNÇÃO MULTIPLICAÇÃO COM ARROW FUNCTION:
+const multiplicaNumeros = (numero1: number, numero2: number): number => numero1 * numero2;
+
+console.log(multiplicaNumeros(5, 8));
+
+
+//FUNÇÃO DE SAUDAÇÃO:
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(saudacao('Jade'));
